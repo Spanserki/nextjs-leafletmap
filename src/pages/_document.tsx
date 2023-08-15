0e6be4ba3b0443e337c56488242bbdfc59e99f16
Spanserki <guilhermespanserkiohc@hotmail.com>
2023-08-14 22:16:11 -0300

    Init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
       <Head />
       <body>
         <Main />
